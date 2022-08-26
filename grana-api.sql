85d3417f483720c4476b4f3a919245b7d9426c68
And-P <andrexpaiva@gmail.com>
2022-08-26 00:10:44 -0300

    Add HTTP:Methods Delete/Put para Pessoa e Put para propriedade ativo em Pessoa e tratando Exceptions

--- grana-api.sql
+++ grana-api.sql
@@ -1,11 +1,15 @@
 show databases;
 use granapi;
 show tables;
-select * from flyway_schema_history;
-select * from categoria;
-delete from categoria where codigo = 15;
+
 describe categoria;
 describe pessoa;
+
+select * from flyway_schema_history;
+select * from categoria;
+select * from pessoa;
+
+delete from categoria where codigo = 15;
 
 INSERT INTO categoria (nome) values ('Lazer');
 INSERT INTO categoria (nome) values ('Alimentação');
